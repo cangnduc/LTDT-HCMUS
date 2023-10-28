ef33fc0b6617a258f507c6546b6a19c6b0630cc0
cangnduc <cang.ngyenduc@gmail.com>
2023-10-28 23:47:26 +0700

    update cau 2

--- Bai2.cs
+++ Bai2.cs
@@ -5,4 +5,77 @@ class Bai2
     {
         g = new Graphs(filePath);
     }
+    // Đếm số thành phần liên thông của đồ thị và in ra mỗi thành phần liên thông
+
+    public void ShowBai2()
+    {
+        Console.WriteLine("Bai 2, Thanh phan lien thong: ");
+        if (g.IsConnected())
+        {
+            Console.WriteLine("Do thi lien thong");
+            DFS(0, new bool[g.V]);
+        }
+        else
+        {
+            Console.WriteLine("Do thi khong lien thong");
+            List<List<int>> connectedComponents = ConnectedComponents();
+            Console.WriteLine("So thanh phan lien thong: " + connectedComponents.Count);
+            for (int i = 0; i < connectedComponents.Count; i++)
+            {
+                Console.Write("Thanh phan lien thong thu " + (i + 1) + ": ");
+                for (int j = 0; j < connectedComponents[i].Count; j++)
+                {
+                    Console.Write(connectedComponents[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public List<List<int>> ConnectedComponents()
+    {
+        List<List<int>> connectedComponents = new List<List<int>>();
+        bool[] visited = new bool[g.V];
+        for (int i = 0; i < g.V; i++)
+        {
+            visited[i] = false;
+        }
+        for (int i = 0; i < g.V; i++)
+        {
+            if (!visited[i])
+            {
+                List<int> connectedComponent = new List<int>();
+                DFS(i, visited, connectedComponent);
+                connectedComponents.Add(connectedComponent);
+            }
+        }
+        return connectedComponents;
+    }
+    public void DFS(int u, bool[] visited, List<int> connectedComponent)
+    {
+        visited[u] = true;
+        connectedComponent.Add(u);
+        for (int v = 0; v < g.V; v++)
+        {
+            if (!visited[v] && g.adjMatrix[u, v].Count > 0)
+            {
+                DFS(v, visited, connectedComponent);
+            }
+        }
+    }
+    // Giải thuật DFS, input là đỉnh bắt đầu
+    // in ra đường đi từ đỉnh bắt đầu đến tất cả các đỉnh
+    public void DFS(int start, bool[] visited)
+    {
+        visited[start] = true;
+        Console.Write(start + " ");
+        for (int v = 0; v < g.V; v++)
+        {
+            if (!visited[v] && g.adjMatrix[start, v].Count > 0)
+            {
+                DFS(v, visited);
+            }
+        }
+    }
+   
+
 }
--- Program.cs
+++ Program.cs
@@ -8,16 +8,19 @@ namespace LTDT_Project
         {
             //string filePath = "graph/djtra.txt";
             //string filePath = "graph/djtra-3.txt";
-            string filePath = "graph/Bai3-1.txt";
+            //string filePath = "graph/Bai3-1.txt";
+            string filePath = "graph/Bai2-3.txt"; // Bai 2 ví dụ 3, in các thành phần liên thông
             Bai1 bai1 = new Bai1(filePath);
             bai1.ShowBai1();
-            
+
             Bai5 bai5 = new Bai5(filePath);
             bai5.ShowBai5();
             Bai4 bai4 = new Bai4(filePath);
             bai4.ShowBai4();
             Bai3 bai3 = new Bai3(filePath);
             bai3.ShowBai3();
+            Bai2 bai2 = new Bai2(filePath);
+            bai2.ShowBai2();
         }
 
     }
